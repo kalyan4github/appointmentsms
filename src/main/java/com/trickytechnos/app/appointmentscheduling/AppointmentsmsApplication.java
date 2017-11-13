cda1ea794d4d2c1d60c56f52fab17a30f412008f
kalyan <kalyaninform@gmail.com>
2017-11-14 02:01:51 +0530

    First commit after project setup

---
+++ src/main/java/com/trickytechnos/app/appointmentscheduling/AppointmentsmsApplication.java
@@ -0,0 +1,13 @@
+package com.trickytechnos.app.appointmentscheduling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointmentsmsApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(AppointmentsmsApplication.class, args);
+	}
+}
